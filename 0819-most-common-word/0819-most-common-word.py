0ea972fb2f9e0a3336816ee4980f618ff3197db4
vvoo_ds <48902356+JungWooChul@users.noreply.github.com>
2023-08-07 20:13:44 +0900

    Time: 46 ms (75.94%), Space: 16.2 MB (99.08%) - LeetHub

--- 0819-most-common-word/0819-most-common-word.py
+++ 0819-most-common-word/0819-most-common-word.py
@@ -1,25 +1,32 @@
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
-        tmp_lst = []
+        words = [word for word in re.sub(r'[^\w]', ' ', paragraph).lower().split() if word not in banned]
         
-        # paragraph 분리
-        for i in paragraph.replace(',',' ').split(' '):
-            tmp = re.sub('[^a-zA-Z]','',i) # 단어에서 영어만 남기기
-            tmp_lst.append(tmp.lower()) # 소문자로 저장
+        counts = collections.defaultdict(int)
+        for word in words:
+            counts[word] += 1
         
-        # 금지된 단어가 아닌 단어만 포함
-        tmp_lst = [tmp for tmp in tmp_lst if tmp not in banned]
+        return max(counts, key = counts.get)
+#         tmp_lst = []
         
-        # 단어 - 단어 횟수 mapping
-        count_dict = {i:0 for i in set(tmp_lst)}
-        for i in tmp_lst:
-            count_dict[i] += 1
+#         # paragraph 분리
+#         for i in paragraph.replace(',',' ').split(' '):
+#             tmp = re.sub('[^a-zA-Z]','',i) # 단어에서 영어만 남기기
+#             tmp_lst.append(tmp.lower()) # 소문자로 저장
+        
+#         # 금지된 단어가 아닌 단어만 포함
+#         tmp_lst = [tmp for tmp in tmp_lst if tmp not in banned]
+        
+#         # 단어 - 단어 횟수 mapping
+#         count_dict = {i:0 for i in set(tmp_lst)}
+#         for i in tmp_lst:
+#             count_dict[i] += 1
     
-        # 정답 고르기
-        max = 0
-        for k,v in count_dict.items():
-            if max < v and len(k)>0:
-                answer = k
-                max = v
+#         # 정답 고르기
+#         max = 0
+#         for k,v in count_dict.items():
+#             if max < v and len(k)>0:
+#                 answer = k
+#                 max = v
         
-        return answer
+#         return answer
