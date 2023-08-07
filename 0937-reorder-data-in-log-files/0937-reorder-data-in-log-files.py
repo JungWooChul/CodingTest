5fb1edf624565da002fadd102a2352f0e6de7b19
vvoo_ds <48902356+JungWooChul@users.noreply.github.com>
2023-08-07 19:52:08 +0900

    Time: 57 ms (30.32%), Space: 16.4 MB (68.33%) - LeetHub

--- 0937-reorder-data-in-log-files/0937-reorder-data-in-log-files.py
+++ 0937-reorder-data-in-log-files/0937-reorder-data-in-log-files.py
@@ -5,8 +5,10 @@ class Solution:
         for i in logs:
             log = i.split(' ')[0]
             words = i.split(' ')[1:]
+            # 숫자 로그는 입력 순서대로 저장
             if words[0].isdigit():
                 dig_log.append(i) 
+            # 문자 로그는 로그를 제외한 부분을 key, 로그를 value로 mapping하여 저장
             else:
                 if ' '.join(words) in let_log.keys():
                     let_log[' '.join(words)].append(log)
@@ -14,9 +16,7 @@ class Solution:
                     let_log[' '.join(words)] = [log]
         
         answer = []
-        for let in sorted(let_log.keys()):
-            for let_tmp in sorted(let_log[let]):
+        for let in sorted(let_log.keys()): # 로그를 제외한 부분 먼저 정렬
+            for let_tmp in sorted(let_log[let]): # 문자가 같다면 로그 정렬
                 answer.append(let_tmp+' '+let) # 문자로 구성된 로그는 식별
-        answer.extend(dig_log) # 숫자 로그는 입력 순서대로
-        
-        return answer
+        return answer + dig_log # 숫자 로그는 입력 순서대로
