b1f1e60b170a67da012c8a7bb00f7ae145cf159a
vvoo_ds <48902356+JungWooChul@users.noreply.github.com>
2023-08-13 19:19:15 +0900

    Time: 96 ms (91.84%), Space: 20.2 MB (57.12%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -9,15 +9,4 @@ class Solution:
             anagrams_dict[tmp_key].append(anagram)
         
         return anagrams_dict.values()
-#         anagram_dict = {str : ''.join(sorted(list(str))) for str in strs}
-#         # list로는 딕셔너리의 key가 존재하는지 판단 불가능 -> 문자열로 다시 합치기
-#         answers = dict()
-#         for k, v in anagram_dict.items():
-#             if v in answers.keys():
-#                 answers[v].append(k)
-#             else:
-#                 answers[v] = [k]
-        
-#         answer = [v for v in answers.values()]
-#         return answer
           
