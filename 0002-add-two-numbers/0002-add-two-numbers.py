960ed6a40a7509e6b0dad881f44d8ca1583000a0
vvoo_ds <48902356+JungWooChul@users.noreply.github.com>
2023-09-02 15:22:32 +0900

    Time: 49 ms (99.52%), Space: 16.5 MB (25.63%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,27 +5,35 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        check = 0
+        answer = ListNode()
+        head = answer
+        while l1 and l2:
+            tmp = l1.val + l2.val + check
+            head.next = ListNode(tmp%10)
+            head = head.next
+            check = tmp // 10
+            
+            l1, l2 = l1.next, l2.next
+        
+        if l1:
+            while l1:
+                tmp = l1.val + check
+                head.next = ListNode(tmp%10)
+                head = head.next
+                check = tmp//10
 
-        l1_sum, l1_check = 0, 0
-        while l1 != None:
-            l1_sum += int(l1.val) * (10**l1_check)
-            l1_check += 1
-            l1 = l1.next
+                l1 = l1.next
+        if l2:
+            while l2:
+                tmp = l2.val + check
+                head.next = ListNode(tmp%10)
+                head = head.next
+                check = tmp//10
+
+                l2 = l2.next
+        if check:
+            head.next = ListNode(check)
+            head = head.next
+        return answer.next
         
-        l2_sum, l2_check = 0, 0
-        while l2 != None:
-            l2_sum += int(l2.val) * (10**l2_check)
-            l2_check += 1
-            l2 = l2.next
-        
-        answer_sum = l1_sum + l2_sum
-        answer_head = ListNode(answer_sum%10)
-        answer_tail = answer_head
-        answer_sum = answer_sum//10
-        while answer_sum > 0:
-            newnode = ListNode(answer_sum%10)
-            answer_tail.next = newnode
-            answer_tail = answer_tail.next
-            answer_sum = answer_sum//10
-        
-        return answer_head
