a859d78fd726b5242cefe7e4e715ebf59290e345
vvoo_ds <48902356+JungWooChul@users.noreply.github.com>
2023-09-04 19:43:08 +0900

    Time: 63 ms (79.17%), Space: 16.4 MB (63.22%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -1,37 +1,39 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    # 이전 회차에서 진행했던 연결리스트의 역순을 취하는 함수
-    def reverseList(self, head: ListNode) -> ListNode:
-        node, prev = head, None
-
-        while node:
-            next, node.next = node.next, prev
-            prev, node = node, next
-
-        return prev
-
-    # 노드를 반복하며 값을 리스트에 추가하는 함수
-    def toList(self, node: ListNode) -> ListNode:
-        list: List = []
-        while node:
-            list.append(node.val)
-            node = node.next
-        return list
-
-    # 파이썬의 리스트를 역순 연결 리스트로 반환
-    def toReversedLinkedList(self, result: ListNode) -> ListNode:
-        prev: ListNode = None
-        for r in result:
-            node = ListNode(r)
-            node.next = prev
-            prev = node
-        
-        return node
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        # 연결리스트를 뒤집고 파이썬의 리스트 형태로 값 변환
-        a = self.toList(self.reverseList(l1))
-        b = self.toList(self.reverseList(l2))
+        check = 0
+        answer = ListNode()
+        head = answer
+        while l1 and l2:
+            tmp = l1.val + l2.val + check
+            head.next = ListNode(tmp%10)
+            head = head.next
+            check = tmp // 10
+            
+            l1, l2 = l1.next, l2.next
         
-        # 덧셈 진행
-        resultStr = int(''.join(str(e) for e in a)) + int(''.join(str(e) for e in b))
+        if l1:
+            while l1:
+                tmp = l1.val + check
+                head.next = ListNode(tmp%10)
+                head = head.next
+                check = tmp//10
 
-        return self.toReversedLinkedList(str(resultStr))
+                l1 = l1.next
+        if l2:
+            while l2:
+                tmp = l2.val + check
+                head.next = ListNode(tmp%10)
+                head = head.next
+                check = tmp//10
+
+                l2 = l2.next
+        if check:
+            head.next = ListNode(check)
+            head = head.next
+        return answer.next
+        
