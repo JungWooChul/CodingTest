ae72b7463bfbc3d62324a593a988d4b2d3b6ad23
vvoo_ds <48902356+JungWooChul@users.noreply.github.com>
2023-10-15 18:48:12 +0900

    Time: 88 ms (95.28%), Space: 21.1 MB (58.60%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,7 +1,12 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        answer = [i[0] for i in collections.Counter(nums).most_common(k)]
-        return answer
+        hashmap = collections.defaultdict(int)
+        for num in nums:
+            hashmap[num] += 1
+        
+        # hashmap의 value를 기준으로 정렬
+        sorted_hashmap = sorted(hashmap.items(), key=lambda x: x[1], reverse=True)
+        return [k[0] for k in sorted_hashmap[:k]]
         
         
         
