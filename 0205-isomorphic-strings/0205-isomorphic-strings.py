e2fbcde2e25605b911e074f874705ef10177d308
vvoo_ds <48902356+JungWooChul@users.noreply.github.com>
2024-01-20 19:10:38 +0900

    Time: 49 ms (39.94%), Space: 17.4 MB (51.06%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,13 +1,51 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        hash_t = {}
-        for k,v in zip(s,t):
-            if k in hash_t.keys():
-                if hash_t[k]!=v:
-                    return False
-            else:
-                if v in hash_t.values():
-                    return False
-                hash_t[k] = v
+        s_lst = [0]*len(s)
+        t_lst = [0]*len(t)
+        
+        my_dict = {}
+        cnt = 0
 
-        return True
+        s_lst[0]=1
+        t_lst[0]=1
+        my_dict[s[0]]=t[0]
+        
+        for i in range (len(s)):
+                if s[i] not in my_dict.keys():
+                    my_dict[s[i]]=t[i]
+        
+        my_dict_rev = {}
+        for k,v in my_dict.items():
+            if v not in my_dict_rev:
+                my_dict_rev[v] = k
+        print(my_dict_rev)
+        for i in range(1,len(s)):
+            
+            if s[i] == s[i-1] and t[i] == t[i-1]:
+                s_lst[i] = s_lst[i-1] + 1
+                t_lst[i] = t_lst[i-1] + 1
+                
+  
+            else:
+                if t[i] == my_dict[s[i]] and s[i] == my_dict_rev[t[i]]:
+                    s_lst[i] =  1
+                    t_lst[i] =  1
+                else:
+                    t_lst[i] = 1000
+        print(s_lst,t_lst)
+            
+        if s_lst == t_lst:
+            return True
+# class Solution:
+#     def isIsomorphic(self, s: str, t: str) -> bool:
+#         hash_t = {}
+#         for k,v in zip(s,t):
+#             if k in hash_t.keys():
+#                 if hash_t[k]!=v:
+#                     return False
+#             else:
+#                 if v in hash_t.values():
+#                     return False
+#                 hash_t[k] = v
+
+#         return True
